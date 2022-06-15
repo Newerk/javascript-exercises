445fd3a80ba9bf36700a2ff38b6f903b285dafeb
Newerk <juniusowens@yahoo.com>
2022-06-15 11:13:52 -0400

    Add Completed Attempt for Test Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,26 +1,68 @@
-const findTheOldest = function(people) {//returns the oldest person by name
+const findTheOldest = function(people) {
     for (const iterator of people) {
+        addDeathYear(people);
 
-        if((iterator.yearOfDeath-iterator.yearOfBirth) === oldestAgeNumber(people)){
+        if((iterator.yearOfDeath-iterator.yearOfBirth) === getOldestAge(people)){
             return iterator;
     }
 
 }
 };
 
-const oldestAgeNumber = (array) => {
+//returns the oldest age out of the list of objects
+const getOldestAge = (array) => {
     let result = [];
     let max;
-    let currentYear = 2020;
   
       for (const iterator of array) {
         result.push(iterator.yearOfDeath - iterator.yearOfBirth);
         max = Math.max(...result);
 
       }
-        return max;//returns the largest age out of the list of objects
+        return max;
+  };
+
+  const addDeathYear = (array) => {
+      const currentYear = new Date();// year doesnt need to be manually updated as time passes
+
+      for (let i = 0; i < array.length; i++) {
+          if (Object.keys(array[i]).length < 3) { //when length of element is less than 3, add a year of death key: value
+              array[i].yearOfDeath = currentYear.getFullYear();
+              
+          }
+
+          
+      }
+
+      return array;
+
+
   };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+const test = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+      yearOfDeath: 2022
+
+    },
+    {
+      name: "Ray", 
+      yearOfBirth: 1962//,
+    //   yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+// console.log(findTheOldest(test));
+// console.log(getOldestAge(test));
+// console.log(addDeathYear(test));
+
 
